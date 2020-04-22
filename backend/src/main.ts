bd1bd8a5e173c73c67814fa5866237de312cc687
Xavier Corbeil <xcorbeil@hotmail.com>
2020-04-22 11:55:09 -0400

    env dev and prod

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3003
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
